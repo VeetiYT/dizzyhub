c29d08583f837a346f1b107e715bd8c5eebcfc4f
VeetiYT <91524659+VeetiYT@users.noreply.github.com>
2022-08-09 20:01:52 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 1962086868 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VeetiYT/dizzyhub/main/towerofhell.lua"))()
+if game.PlaceId == 9049840490 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VeetiYT/dizzyhub/main/sonicspeedsim.lua"))()
